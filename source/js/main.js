b6cdd84fd552bc39a4ef7dc139c5d3cd3b076daf
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2024-12-15 21:54:00 +0300

    Реализует хедер для мобилки с бургерным меню

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,19 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+/* Menu*/
+const header = document.querySelector('.header');
+const headerToggle = document.querySelector('.header__burger-button');
+
+header.classList.remove('header--nojs');
+
+headerToggle.addEventListener('click', function () {
+  if (header.classList.contains('header--closed')) {
+    header.classList.remove('header--closed');
+    header.classList.add('header--opened');
+  } else {
+    header.classList.add('header--closed');
+    header.classList.remove('header--opened');
+  }
+});
