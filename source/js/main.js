03cc13798833e6bc04f38b74c993e8946e500f60
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2024-12-19 23:43:57 +0300

    добавляет блок hero

--- source/js/main.js
+++ source/js/main.js
@@ -6,15 +6,18 @@
 /* Menu*/
 const header = document.querySelector('.header');
 const headerToggle = document.querySelector('.header__burger-button');
-
-header.classList.remove('header--nojs');
+const overlay = document.querySelector('.overlay');
 
 headerToggle.addEventListener('click', function () {
   if (header.classList.contains('header--closed')) {
     header.classList.remove('header--closed');
     header.classList.add('header--opened');
+    overlay.classList.add('is-active');
   } else {
     header.classList.add('header--closed');
     header.classList.remove('header--opened');
+    overlay.classList.remove('is-active');
   }
 });
+
+
